5108c0bd6c433d656f0f2addd56404dedfa945fa
dineshchintu <mdinesh@iitg.ac.in>
2024-06-14 01:44:10 +0530

    Check the function.

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,66 +1,34 @@
 import React, { useState, useEffect } from "react";
 
 const Stopwatch = () => {
-  
 
-
-  const [time, setTime] = useState(0);
-
+const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
     let intervalId;
     if (isRunning) {
-     
-      intervalId = setInterval(() => setTime(time + 1), 10);
+      intervalId = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 10);
     }
     return () => clearInterval(intervalId);
   }, [isRunning]);
 
-
   const minutes = Math.floor((time % 360000) / 6000);
-console.log(minutes)
+  console.log(minutes);
   
   const seconds = Math.floor((time % 6000) / 100);
-console.log(seconds)
-  
+  console.log(seconds);
+
   const startAndStop = () => {
     setIsRunning(!isRunning);
   };
 
- 
   const reset = () => {
+    setIsRunning(false);
     setTime(0);
-    startAndStop();
   };
-
-// const [time, setTime] = useState(0);
-//   const [isRunning, setIsRunning] = useState(false);
-
-//   useEffect(() => {
-//     let intervalId;
-//     if (isRunning) {
-//       intervalId = setInterval(() => {
-//         setTime(prevTime => prevTime + 1);
-//       }, 10);
-//     }
-//     return () => clearInterval(intervalId);
-//   }, [isRunning]);
-
-//   const minutes = Math.floor((time % 360000) / 6000);
-//   console.log(minutes);
-  
-//   const seconds = Math.floor((time % 6000) / 100);
-//   console.log(seconds);
-
-//   const startAndStop = () => {
-//     setIsRunning(!isRunning);
-//   };
-
-//   const reset = () => {
-//     setIsRunning(false);
-//     setTime(0);
-//   };
   return (
     <div>
         <h1>Stopwatch</h1>
